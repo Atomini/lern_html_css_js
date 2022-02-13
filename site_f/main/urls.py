2ed981b44b200fea5824726044b45a75a7b0fa65
Atomini <62155406+Atomini@users.noreply.github.com>
2022-02-13 18:08:22 +0200

    Начата страница Shop для Барбер, закончена навигация футер, производители

--- site_f/main/urls.py
+++ site_f/main/urls.py
@@ -4,5 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('barber/', barber_index, name='barber_index'),
+    path('barber/shop/', barber_shop, name='barber_shop'),
+
     path('device/', index_device, name='index_device'),
 ]
--- site_f/main/views.py
+++ site_f/main/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 
 
 # Create your views here.
-
 def index(request):
+    return render(request, 'index.html')
+
+
+# barber
+def barber_index(request):
     return render(request, 'barber/index.html')
 
 
+def barber_shop(request):
+    return render(request, 'barber/shop.html')
+
+
+# device
 def index_device(request):
     return render(request, 'device/index.html')
